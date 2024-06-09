f857d9dcf80f09a1922cdf8fb15b66f421eb7f07
stadoo <t.grimmen@gmail.com>
2024-06-09 19:20:27 +0200

    postfixtures corrected

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Post;
 use App\Entity\Category;
+use App\Entity\User;
 use Faker\Factory;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -13,9 +14,8 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 class PostFixtures extends Fixture implements DependentFixtureInterface
 {   
     private $parameters;
-        
-    public function __construct(ParameterBagInterface $parameters)
-    {
+
+    public function __construct(ParameterBagInterface $parameters) {
         $this->parameters = $parameters;
     }
 
@@ -28,29 +28,30 @@ class PostFixtures extends Fixture implements DependentFixtureInterface
 
         $faker = Factory::create();
 
-        $authorIds = [1, 2];
-
         for ($i = 1; $i <= 100; $i++) {
             $newPost = new Post();
-            //$newPost->setAuthorid($authorIds[array_rand($authorIds)]);
-            $newPost->setAuthorid(rand(1,10));
+            $randomUserId = rand(1, 10);
+            $author = $manager->getRepository(User::class)->find($randomUserId);
+
+            if ($author) {
+                $newPost->setAuthor($author);
+            } else {
+                $defaultAuthor = $manager->getRepository(User::class)->find(1);
+                $newPost->setAuthor($defaultAuthor);
+            }
             $newPost->setName($faker->sentence());
             $newPost->setText($faker->paragraph());
             $newPost->setStatus(1);
-            $newPost->setDate($faker->dateTimeBetween('-30 days', ''));
+            $newPost->setDate($faker->dateTimeBetween('-30 days', 'now'));
             $newPost->setLikeCount(0);
             $newPost->setDislikeCount(0);
-
-
-
-            // Zufällige Kategorie aus der Datenbank abrufen und setzen
             $categoryId = $faker->randomElement([1, 2, 3]);
             $category = $manager->getRepository(Category::class)->find($categoryId);
             if ($category) {
                 $newPost->setCategory($category);
             } else {
-                $category = $manager->getRepository(Category::class)->find(1);
-                $newPost->setCategory($category);
+                $defaultCategory = $manager->getRepository(Category::class)->find(1);
+                $newPost->setCategory($defaultCategory);
             }
 
             $manager->persist($newPost);
