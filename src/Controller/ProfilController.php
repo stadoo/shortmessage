67ae6574b550e331a6cada9b6791185f5b92f2cd
stadoo <t.grimmen@gmail.com>
2024-07-03 21:20:11 +0200

    add sociallink function

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -10,9 +10,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\PostRemove;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -20,7 +22,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
-  #[Route('/user/profil',name:'user_profil')]
+  #[Route('/user/profile',name:'user_profile')]
   public function index(EntityManagerInterface $em ,PostRepository $postRepository, PaginatorInterface $paginator, Request $request):Response {
     $posts = $em->getRepository(Post::class)->findBy(['author' => $this->getUser()]);
 
@@ -30,7 +32,7 @@ class ProfilController extends AbstractController
         10 /*limit per page*/);
         $pagination->setTemplate('home/my_pagination.html.twig');
         
-    return $this->render('profil/userprofil.html.twig', [
+    return $this->render('profile/userprofile.html.twig', [
       'posts'=>$posts,
       'pagination' => $pagination,
 
@@ -51,11 +53,95 @@ class ProfilController extends AbstractController
         $pagination->setTemplate('home/my_pagination.html.twig');
 
 
-    return $this->render('profil/userview.html.twig', [
+    return $this->render('profile/userview.html.twig', [
       'user'=>$user,
       'posts'=>$posts,
       'pagination' => $pagination,
 
   ]);
   }
+
+    #[Route('/user/profile/edit',name:'user_edit')]
+    public function editProfile(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+
+        if (is_null($user->getSocialLinks())) {
+            $user->setSocialLinks([]);
+        }
+
+        $formSocialLink = $this->createFormBuilder()
+            ->add('newSocialLink', UrlType::class, [
+                'label' => 'New Social Link',
+                'attr' => ['placeholder' => 'https://']
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Add'])
+            ->getForm();
+
+        $formSocialLink->handleRequest($request);
+
+        $fromPassword = $this->createFormBuilder()
+        ->add('password', PasswordType::class, [
+            'attr' => array('class' => 'form-control')
+        ])
+        ->add('save', SubmitType::class, [
+            'attr' => array('class' => 'btn btn-outline-danger btn-sm')
+        ])
+        ->getForm();
+
+        $fromPassword->handleRequest($request);
+
+        if ($formSocialLink->isSubmitted() && $formSocialLink->isValid()) {
+            $newLink = $formSocialLink->get('newSocialLink')->getData();
+
+            if (count($user->getSocialLinks()) < 3 && preg_match('/(twitter\.com|x\.com|facebook\.com|linkedin\.com)/', $newLink)) {
+                $user->addSocialLink($newLink);
+
+                $em->persist($user);
+                $em->flush();
+
+                $this->addFlash('success', 'Link added successfully!');
+            } else {
+                $this->addFlash('failure', 'You cannot add more than 3 social links or the link is invalid.');
+            }
+
+            return $this->redirectToRoute('user_edit');
+        } elseif($fromPassword->isSubmitted() && $fromPassword->isValid()) {
+
+            $hashedPassword = $this->userPasswordHasher->hashPassword($user, $fromPassword->get('password')->getData());
+            $user->setPassword($hashedPassword);
+
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'The User Password has been edited successfully!');
+            return $this->redirect($this->generateUrl('home'));
+            
+        }
+
+        return $this->render('profile/useredit.html.twig', [
+            'formSocialLink' => $formSocialLink->createView(),
+            'formEditPassword' => $fromPassword->createView(),
+            'user' => $user
+        ]);
+    }
+
+
+    #[Route('/profile/remove-link/{index}', name:'remove_social_link')]
+    public function removeSocialLink(int $index, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $links = $user->getSocialLinks();
+
+        if (isset($links[$index])) {
+            unset($links[$index]);
+            $user->setSocialLinks(array_values($links)); // Reindex array
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Link removed successfully!');
+        }
+
+        return $this->redirectToRoute('user_edit');
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -11,6 +13,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[Assert\Callback('App\Entity\User', 'validateSocialLinks')]
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -33,6 +37,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    #[ORM\Column(type:'json', nullable: true)]
+    #[Assert\Count(max:3, maxMessage:'You cannot specify more than 3 social links.')]
+    private $socialLinks = [];
+
     //for password-reset
     #[ORM\Column(type:'string', length:255, nullable:true)]
     private $resetToken;
@@ -41,6 +49,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type:'datetime', nullable:true)]
     private $tokenExpirationDate;
 
+    public function __construct()
+    {
+        $this->socialLinks = [];
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
@@ -100,6 +113,51 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->password = $password;
 
         return $this;
+    }
+
+    public function getSocialLinks(): array
+    {
+        return is_array($this->socialLinks) ? $this->socialLinks : [];
+    }
+
+    public function setSocialLinks(?array $socialLinks): self
+    {
+        $this->socialLinks = $socialLinks ?? [];
+
+        return $this;
+    }
+
+    public function addSocialLink(string $link): self
+    {
+        $socialLinks = $this->getSocialLinks();
+        if (count($socialLinks) < 3) {
+            $this->socialLinks[] = $link;
+        } else {
+            throw new \Exception('You cannot add more than 3 social links.');
+        }
+
+        return $this;
+    }
+
+    public function removeSocialLink(string $link): self
+    {
+        if (($key = array_search($link, $this->socialLinks)) !== false) {
+            unset($this->socialLinks[$key]);
+            $this->socialLinks = array_values($this->socialLinks); // Reindex array
+        }
+
+        return $this;
+    }
+
+    public static function validateSocialLinks(array $socialLinks, ExecutionContextInterface $context): void
+    {
+        foreach ($socialLinks as $link) {
+            if (!preg_match('/(twitter\.com|x\.com|facebook\.com|linkedin\.com)/', $link)) {
+                $context->buildViolation('Links must be from Twitter, Facebook, or Linkedin.')
+                    ->atPath('socialLinks')
+                    ->addViolation();
+            }
+        }
     }
 
     /**
